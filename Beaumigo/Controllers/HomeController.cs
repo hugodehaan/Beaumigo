6b24f6bb856e6679eef61eefcf29ecef28742223
Madelief van der Bles <110461@st-maartenscollege.nl>
2022-03-07 14:04:55 +0100

    mislukte nav

--- Beaumigo/Controllers/HomeController.cs
+++ Beaumigo/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Beaumigo.Controllers
             return View();
         }
 
+        public IActionResult ETEN()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
