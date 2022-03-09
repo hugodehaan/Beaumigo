5f679929bc236f56cbab81c3440ad71dfe871f16
beau rikkoert <110368@st-maartenscollege.nl>
2022-03-09 15:47:34 +0100

    database iets verder 3

--- Beaumigo/Controllers/HomeController.cs
+++ Beaumigo/Controllers/HomeController.cs
@@ -25,47 +25,45 @@ namespace Beaumigo.Controllers
             return View();
         }
 
-        //public List<Product> GetProducts()
-        //{
-        //    // stel in waar de database gevonden kan worden
-        //    string connectionString = "Server=172.16.160.21;Port=3306;Database=110368;Uid=110368;Pwd=inf2021sql;";
+        public List<Product> GetProducts()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110368;Uid=110368;Pwd=inf2021sql;";
 
-        //    // maak een lege lijst waar we de namen in gaan opslaan
-        //    List<Product> products = new List<Product>();
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Product> products = new List<Product>();
 
-        //    // verbinding maken met de database
-        //    using (MySqlConnection conn = new MySqlConnection(connectionString))
-        //    {
-        //        // verbinding openen
-        //        conn.Open();
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
 
-        //        // SQL query die we willen uitvoeren
-        //        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
 
-        //        // resultaat van de query lezen
-        //        using (var reader = cmd.ExecuteReader())
-        //        {
-        //            // elke keer een regel (of eigenlijk: database rij) lezen
-        //            while (reader.Read())
-        //            {
-        //                Product p = new Product
-        //                {
-        //                    // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
-        //                    Id = Convert.ToInt32(reader["Id"]),
-        //                    Beschikbaarheid = Convert.ToInt32(reader["Beschikbaarheid"]),
-        //                    Naam = reader["Naam"].ToString(),
-        //                    Prijs = reader["Prijs"].ToString(),
-        //                };
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        Product p = new Product
+                        {
+                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                            Id = Convert.ToInt32(reader["Id"]),                            
+                            Naam = reader["Naam"].ToString(),
+                        };
 
-        //                // voeg de naam toe aan de lijst met namen
-        //                products.Add(p);
-        //            }
-        //        }
-        //    }
+                        // voeg de naam toe aan de lijst met namen
+                        products.Add(p);
+                    }
+                }
+            }
 
-        //    // return de lijst met namen
-        //    return products;
-        //}
+            // return de lijst met namen
+            return products;
+        }
 
 
         [Route("eten")]
---
+++ Beaumigo/Models/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Beaumigo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+
+    }
+}
