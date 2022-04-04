caf79829d39c6c153c06fb36612349b6286ce525
Madelief van der Bles <110461@st-maartenscollege.nl>
2022-04-04 14:05:43 +0200

    linkje contact en eten

---
+++ Beaumigo/Models/Eten.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Beaumigo.Models
+{
+    public class Eten
+    {
+        public int Id { get; set; }
+
+        public string naam { get; set; }
+
+        public string gang { get; set; }
+
+        public string ingredienten { get; set; }
+
+        public string allergieen { get; set; }
+
+
+
+    }
+}
